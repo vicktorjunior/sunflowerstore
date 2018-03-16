30e01ffcef1248fab735b8f5381c7301ff19c5ea
Victor Junior <vicktor.junior@gmail.com>
2018-03-16 01:00:43 -0300

    feat(sell): added discount column in view and in javascript formula

--- src/main/resources/static/javascripts/programatcc.js
+++ src/main/resources/static/javascripts/programatcc.js
@@ -31,40 +31,103 @@
     });
     
     // updating total selling price according to product value and quantity
-    $('#qtdSelling').change(function () {
+    /*$('#qtdSelling').change(function () {
         var qtdSelling = parseInt($('#qtdSelling').val());
         var sellingPrice = parseFloat($('#sellingPrice').val());
         $('#sellingTotal').val(qtdSelling*sellingPrice);
     });
+*/
+var totals=0;
+    $('#addrow').click(function () {
+        var qtdSelling = parseInt($('#qtdSelling').val());
+        var sellingPrice = parseFloat($('#sellingPrice').val());
+        var productName = $('#products :selected').text();
+        var discount = parseFloat($('#desconto').val());
+        var sellingTotal = (qtdSelling*sellingPrice)-((discount/100)-sellingPrice);
+
+        console.log(discount/100);
+        console.log(sellingTotal);
 
 
+        var chtml = "<tr>\n" +
+            "                        <td> " + qtdSelling + "</td>\n" +
+            "                        <td> " + productName + "</td>\n" +
+            "                        <td>" + sellingPrice +" </td>\n" +
+            "                        <td>" + discount +" </td>\n" +
+            "                        <td class=\"sellingTotal\">" + sellingTotal +" </td>\n" +
+            "                        <td><div class=\"col-sm-4\"><input type=\"button\" id=\"delete\" class=\"form-control btn btn-danger\" value=\"Delete\"/></div></td>\n" +
+            "                    </tr>";
 
-    $(document).ready(function () {
-        var counter = 0;
+        $('#productsToSell').find('> tbody').append(chtml);
 
-        $("#addrow").on("click", function () {
-            var newRow = $("<tr>");
-            var cols = "";
 
-            cols += '<td><input type="text" class="form-control" name="name' + counter + '"/></td>';
-            cols += '<td><input type="text" class="form-control" name="mail' + counter + '"/></td>';
-            cols += '<td><input type="text" class="form-control" name="phone' + counter + '"/></td>';
+        var $dataRows=$("#productsToSell").find("tr:has('.sellingTotal')");
 
-            cols += '<td><input type="button" class="ibtnDel btn btn-md btn-danger "  value="Delete"></td>';
-            newRow.append(cols);
-            $("table.order-list").append(newRow);
-            counter++;
+        $dataRows.each(function() {
+            $(this).find('.sellingTotal').each(function(){
+                totals+=parseFloat( $(this).html());
+            });
         });
+        /*$("#sum_table td.totalCol").each(function(i){
+            $(this).html("total:"+totals[i]);
+        });*/
 
+        parseFloat($('#sellingTotal').val(totals));
 
-
-        $("table.order-list").on("click", ".ibtnDel", function (event) {
-            $(this).closest("tr").remove();
-            counter -= 1
+        $(".delete").click(function() {
+            $(this).parents("tr").remove();
         });
-
-
     });
+
+
+
+
+    $('#ccnt').data('count', 0);
+    /*$('#addrow').click(function () {
+
+        function getcount() {
+            var $this = $('#ccnt'),
+                count = $this.data('count') + 1;
+
+            $this.data('count', count);
+            return count;
+        }
+
+        var mycount = getcount();
+        //console.log('mycount: ' + mycount);
+
+        var tdclass;
+        if (mycount % 2 == 1) {
+            tdclass = "td1";
+        } else {
+            tdclass = "td2";
+        }
+        //console.log('tdclass: ' + tdclass);
+        //window.alert(mycount + ' ' + tdclass);
+
+        var chtml = "<tr>\n" +
+            "                        <th scope=\"row\">1</th>\n" +
+            "                        <td></td>\n" +
+            "                        <td></td>\n" +
+            "                        <td>1.20</td>\n" +
+            "                        <td><div class=\"col-sm-4\"><input type=\"button\" id=\"delete\" class=\"form-control btn btn-danger\" value=\"Delete\"/></div></td>\n" +
+            "                    </tr>";
+
+
+        //console.log('chtml is: ' + chtml);
+        //window.alert(chtml);
+        //console.log("Writing an iteration of chtml to scrren...");
+        $('#productsToSell').find('> tbody').append(chtml);
+
+        $(".delete").click(function() {
+            $(this).parents("tr").remove();
+        });
+    });*/
+
+
+
+
+
 
 
 
